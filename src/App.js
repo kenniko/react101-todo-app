8002d531c3ac0120affdc3626024eb5a2b001247
Niko <kenniko@protonmail.com>
2021-04-07 12:42:52 +0700

    The Todo form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import TodoList from "./components/todoList";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
     <div className="App">
       <h1>Todos</h1>
       <TodoList />
+      <TodoForm />
     </div>
   );
 }
